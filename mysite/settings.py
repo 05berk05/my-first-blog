54d04cb91df49cbfea3c00acbe6fa3a6ebcf358f
berk <berktuzcu1997@gmail.com>
2019-06-18 10:39:30 +0300

    test

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,5 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
- STATIC_ROOT = "/home/05berk05/05berk05.pythonanywhere.com/static"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
