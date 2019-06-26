d6abb1fd125e75bc875292eb11422c0fe67b30b9
berk <berktuzcu1997@gmail.com>
2019-06-26 11:59:50 +0300

    additional files and changes for Heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangogirls',
+        'USER': 'name',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -129,8 +133,10 @@ USE_TZ = True
 # DATABASES = {}
 # DATABASES['default'] =  dj_database_url.config(default='postgres://
 #                                                         user:pass@host/db')
-# DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
+#DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
 # DATABASES['default'].update(db_from_env)
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
